ba65a8ad7372f988246a6c2148385b7de1f9bb94
Kai19 <ngyankai@hotmail.com>
2017-08-05 03:39:58 +0000

    add review and review delete function for logged in user

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
     belongs_to(:product)
-    validates :product_id, presence: true
-    validates :user_id, presence: true
+    belongs_to(:user)
+    # validates :user_id, presence: true
     validates :description, presence: true
-    validates :rating, presence: true, inclusion: 1..5
+    validates :rating, presence: true
 end
